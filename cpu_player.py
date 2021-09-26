69a0657e3487bc2f0c1c993586d1d49c1dd3a82a
Adam Abernathy <amabernathy@ku.edu>
2021-09-25 19:32:15 -0500

    started cpu_player class

---
+++ cpu_player.py
@@ -0,0 +1,43 @@
+"""
+cpu_player.py
+
+created: sept 25 2021
+by: Adam Abernathy
+
+description:
+cpu player for battleship
+
+"""
+
+import random
+
+class CPU_Player :
+    """ defalut constructor
+    """
+    def __init__ (self):
+        self.previous_move = {0, 1}
+        self.current_move = {0, 1}
+        print("cpu created")
+
+    """ called when it is cpu turn in game
+    """
+    def make_move (self):
+        # check previous for a hits
+
+        # if hit look around hit for other moves
+
+        # make move
+
+        # if no hit, random number
+
+        # check for previous move There
+
+        # act accordingly
+
+        if True:
+            print("MOVE MADE")
+
+    """ checks previous move for a hit
+    """
+    def check_previous ():
+        return True;
